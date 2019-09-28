5a7ee29da3d238725fb8ecdf14516cbe9b575e5c
freyrdanielsson <freyrdanielsson@gmail.com>
2019-09-28 17:33:38 +0200

    Shoot good

--- cpp/Player.cpp
+++ cpp/Player.cpp
@@ -6,37 +6,30 @@
 #include <vector>
 #include <tuple>
 
-using namespace std;
-
 namespace ducks
 {
-
-int nStates, mEmission;
+int n, m;
 vector<vector<vector<Model>>> speciesModels;
 vector<vector<Model>> birdModels;
-vector<ESpecies> specieGuess(6);
+vector<ESpecies> specieGuess;
+
+tuple<double, int> action;
 
 Player::Player()
 {
-    nStates = 9;   // state: type of birds
-    mEmission = 9; // emission: type of moves
+    n = 9; // state: type of birds
+    m = 9; // emission: type of moves
     vector<vector<vector<Model>>> mdl(6);
     speciesModels = mdl; // Vector of model for each species
 }
 
 std::vector<int> getObsSeq(Bird bird)
 {
-    int cnt = 0;
+    std::vector<int> O(bird.getSeqLength());
+
     for (int i = 0; i < bird.getSeqLength(); i++)
     {
-        if (bird.wasAlive(i))
-            cnt++;
-    }
-    std::vector<int> O(cnt);
-
-    for (int i = 0; i < cnt; i++)
-    {
-        O[i] = bird.getObservation(i);
+        O[i] = bird.wasAlive(i) ? bird.getObservation(i) : 0; // check later...
     }
 
     return O;
@@ -45,41 +38,44 @@ std::vector<int> getObsSeq(Bird bird)
 Action Player::shoot(const GameState &pState, const Deadline &pDue)
 {
 
+    if (pState.getRound() == 0)
+    {
+        speciesModels.clear();
+        vector<vector<vector<Model>>> mdl(6);
+        speciesModels = mdl;
+    }
+
     int nBirds = pState.getNumBirds();
     int openSeason = 100 - nBirds;
 
     if (pState.getBird(0).getSeqLength() == 1)
     {
         // each bird will have openSeason nr of models.
-        vector<vector<Model>> tmp(nBirds, vector<Model>(nBirds, Model(nStates, mEmission)));
+        vector<vector<Model>> tmp(nBirds, vector<Model>(nBirds, Model(n, m)));
         birdModels = tmp;
         cerr << "Start Round " << pState.getRound() << endl;
     }
 
     // For each bird
     int victim = -1;
-    double victimOdds = 0.75;
+    double victimOdds = 0.70;
     int nextBirdMove = -1;
-
     for (int b = 0; b < nBirds; b++)
     {
         Bird bird = pState.getBird(b);
         int nObs = bird.getSeqLength();
-
-        if (nObs >= openSeason && bird.isAlive())
+        if (nObs > openSeason && bird.isAlive())
         {
-
-            Model model(nStates, mEmission);
+            Model model(n, m);
             vector<int> O = getObsSeq(bird);
-
-            vector<vector<double>> Alpha = model.estimate(O);
+            model.estimate(O);
 
             // Collect models for bird.
             birdModels[b][nBirds - 100 + nObs] = model;
 
-            /* if (pState.getRound() > 0)
+            if (pState.getRound() > 0)
             {
-                vector<double> maxSpiece(COUNT_SPECIES);
+                /* vector<double> maxSpiece(COUNT_SPECIES);
                 double norm = 0;
 
                 int specie = -1;
@@ -116,15 +112,16 @@ Action Player::shoot(const GameState &pState, const Deadline &pDue)
                         specie = i;
                     }
                 }
-
-                if (speciesModels[SPECIES_BLACK_STORK].empty() || specie == SPECIES_BLACK_STORK || (maxSpiece[SPECIES_BLACK_STORK] > 0.1))
+                cerr <<  bestOfAllS <<endl;
+                if (specie == SPECIES_BLACK_STORK || (maxSpiece[SPECIES_BLACK_STORK] > 0.2) || (bestOfAllS < 0.24))
                 {
-                    continue;
-                }
+                    return cDontShoot;
+                } */
+                
 
                 int likelyState = model.estimateStateSeq(O);
 
-                tuple<double, int> action = model.getNextEmission(Alpha);
+                action = model.getNextEmission(likelyState);
 
                 if (get<0>(action) >= victimOdds)
                 {
@@ -132,13 +129,19 @@ Action Player::shoot(const GameState &pState, const Deadline &pDue)
                     victim = b;
                     nextBirdMove = get<1>(action);
                 }
-            } */
+            }
         }
     }
-    return cDontShoot;
+
+    /* if (nextBirdMove >= 0)
+    {
+        cerr << victimOdds << endl;
+    } */
+
+    //return cDontShoot;
     //std::cerr << "Round: " << pState.getRound() << " action " << EMovement(nextBirdMove) << " victim " << victim <<endl;
 
-    //return Action(victim, EMovement(nextBirdMove));
+    return Action(victim, EMovement(nextBirdMove));
 }
 
 std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDue)
@@ -149,11 +152,11 @@ std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDu
      */
     specieGuess.resize(pState.getNumBirds(), SPECIES_UNKNOWN);
 
-    if (pState.getRound() == 0)
+    if (pState.getRound() == 0 || true)
     {
         for (int i = 0; i < pState.getNumBirds(); i++)
         {
-            specieGuess[i] = ESpecies(rand() % COUNT_SPECIES);
+            specieGuess[i] = SPECIES_UNKNOWN;
         }
         return specieGuess;
     }
@@ -169,6 +172,7 @@ std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDu
 
         int specie = -1;
         double bestOfAllS = 0;
+
         for (int i = 0; i < COUNT_SPECIES; i++)
         {
             double specieMaxP = 0;
@@ -176,11 +180,9 @@ std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDu
             {
                 for (int k = 0; k < speciesModels[i][j].size(); k++)
                 {
-
                     double tmpSpecieMaxP = speciesModels[i][j][k].estimateEmissionSequence(O);
-                    //double tmpSpecieMaxP = 0;
                     //avg += tmpSpecieMaxP;
-                    if (specieMaxP <= tmpSpecieMaxP && !isinf(tmpSpecieMaxP))
+                    if (specieMaxP < tmpSpecieMaxP && !isinf(tmpSpecieMaxP))
                     {
                         specieMaxP = tmpSpecieMaxP;
                     }
@@ -191,10 +193,10 @@ std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDu
             norm += specieMaxP;
         }
 
-        /* for (int i = 0; i < COUNT_SPECIES; i++)
+        for (int i = 0; i < COUNT_SPECIES; i++)
         {
             maxSpiece[i] /= norm;
-        } */
+        }
 
         for (int i = 0; i < COUNT_SPECIES; i++)
         {
@@ -205,12 +207,12 @@ std::vector<ESpecies> Player::guess(const GameState &pState, const Deadline &pDu
             }
         }
 
-        cerr << "best " << specie << endl;
+        /* cerr << "best " << specie << endl;
         for (int i = 0; i < COUNT_SPECIES; i++)
         {
             cerr << maxSpiece[i] << " ";
         }
-        cerr << endl;
+        cerr << endl; */
 
         specieGuess[b] = ESpecies(specie);
     }
@@ -232,6 +234,9 @@ void Player::reveal(const GameState &pState, const std::vector<ESpecies> &pSpeci
      */
     for (int i = 0; i < pSpecies.size(); i++)
     {
+        // Add all hmm from the round to this specie
+        // pSpecies[i] = specie of bird i in this round
+        // species[i][j] = models for specie j in round i
 
         if (pSpecies[i] == ESpecies(SPECIES_BLACK_STORK))
             cerr << "put in stork, bird nr: " << i << endl;
